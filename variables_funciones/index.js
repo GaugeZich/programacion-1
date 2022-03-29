815938ff7541dfeaafaaa6464b4184905859dc6d
GaugeZich <guilleop.vale@gmail.com>
2022-03-29 17:07:17 -0300

    Constantes, condicionales y funciones

---
+++ variables_funciones/index.js
@@ -0,0 +1,21 @@
+function mostrar_mensaje(){
+    alert("Probando mi funcion")
+}
+function mostrar_en_consola(){
+    const nombre_apellido = 'Guillermo Valenzuela el mas picante recontra pereyra'
+
+    /*Forma 1 - sin template string*/
+
+    console.log('nombresito del pibe: ' +nombre_apellido+ ' uwu (Forma 1)')
+
+    // Forma 2
+    console.log(`nombresito del pebete ${nombre_apellido} uwu (Forma 2)`)
+}
+function validar(){
+    const calificacion=parseInt(prompt('decime la notita a ver como te fue'))
+    if(calificacion>=7){
+        alert('Ta aprobao mi rey')
+    }else{
+        alert('Nos recontra mil vimo en diecembre pa')
+    }
+}
