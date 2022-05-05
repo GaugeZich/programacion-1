f5355e4f24a70dc8a4f4ec9fa7f4694870f95f37
GaugeZich <guilleop.vale@gmail.com>
2022-05-05 16:23:28 -0300

    Arreglo del EJ1

--- Tp Evaluativo/Ej1/main.js
+++ Tp Evaluativo/Ej1/main.js
@@ -4,18 +4,21 @@ function descuento() {
     //Se realiza la condicion a base del contenido guardado en la constante Ford
     if(Ford==1){
         const Ka = (1750000 * 15) / 100
+        const precio= 1750000 - Ka
         //Se realiza el porcentaje y se guarda en una constante
-        document.getElementById("h1_r").textContent = "El precio final sera de: " +Ka+ "\$";
+        document.getElementById("h1_r").textContent = "El precio final sera de: " +precio+ "\$";
         //Se guardara el precio con el descuento en el h1 gracias al textContent
     }else if(Ford==2){
         const Fiesta = (1950000 * 5) / 100
+        const precio= 1950000 - Fiesta
         //Se realiza el porcentaje y se guarda en una constante
-        document.getElementById("h1_r").textContent = "El precio final sera de: " +Fiesta+ "\$";
+        document.getElementById("h1_r").textContent = "El precio final sera de: " +precio+ "\$";
         //Se guardara el precio con el descuento en el h1 gracias al textContent
     }else{
         const Focus = (2560000 * 10) / 100
+        const precio= 2560000 - Focus
         //Se realiza el porcentaje y se guarda en una constante
-        document.getElementById("h1_r").textContent = "El precio final sera de: " +Focus+ "\$";
+        document.getElementById("h1_r").textContent = "El precio final sera de: " +precio+ "\$";
         //Se guardara el precio con el descuento en el h1 gracias al textContent
     }
 }
