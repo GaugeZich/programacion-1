a01f86deff751ffe509734318341bd1e2dda4122
GaugeZich <guilleop.vale@gmail.com>
2022-06-30 17:02:12 -0300

    POO Ej3

--- Poo_ej3/main.js
+++ Poo_ej3/main.js
@@ -1,2 +1,17 @@
 import Producto from "./producto.js";
-const producto = new Producto()
+function guardar(){
+    let descripcion = document.getElementById("inp_desc").value
+    let precio = document.getElementById("inp_precio").value
+    let categoria = document.getElementById("slt_cat").value
+    let producto = new Producto(descripcion,precio,categoria)
+    /*Invocamos(ejecutamos) al metodo guardar_producto(),
+    pertenecientes a la clase Producto
+    */
+    producto.guardar_producto()
+}
+document.getElementById("btn").addEventListener("click",guardar)
+function listar(){
+    let producto = new Producto()
+    producto.obtener_producto()
+}
+listar()
--- Poo_ej3/producto.js
+++ Poo_ej3/producto.js
@@ -1,6 +1,32 @@
 export default class Producto {
-    constructor()
+    constructor(desc,precio,cat){
+        this.descripcion = desc
+        this.precio = precio
+        this.categoria = cat
+    }
     guardar_producto(){
+        let nuevo_producto = {
+            descripcion: this.descripcion,
+            precio: this.precio,
+            categoria: this.categoria
+        }
+        if("productos" in localStorage){
+            //Converitmos el JSON obtenido desde el storage, en una expresion nativa de Javascript
+            let lista = JSON.parse(localStorage.getItem("productos"))
+            lista.push(nuevo_producto)
+            localStorage.setItem("productos",JSON.stringify(lista))
+        }else{
+            let lista = [] 
+            lista.push(nuevo_producto)
+            localStorage.setItem("productos",JSON.stringify(lista))
+        }
+        /*let lista = []
+        lista.push(nuevo_producto)
+        //Guardar en el storage:
+        localStorage.setItem("productos",JSON.stringify(lista))
+        */
+    }
+    obtener_producto(){
         
     }
 }
